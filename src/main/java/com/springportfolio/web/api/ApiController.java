e0380af6f23136f5cb6996a4d51da1069d3da2bd
Home-PC\Home <pakoh200@gmail.com>
2017-05-01 19:15:20 +0900

    package명 변경에따른 import변경

--- src/main/java/com/springportfolio/web/api/ApiController.java
+++ src/main/java/com/springportfolio/web/api/ApiController.java
@@ -37,8 +37,8 @@ public class ApiController {
 		int pageNUM = Integer.parseInt(pnum);
 		int start = (pageNUM-1)*10+1;
 		
-		String clientId = "id";// 애플리케이션 클라이언트 아이디값";
-		String clientSecret = "secret";// 애플리케이션 클라이언트 시크릿값";
+		String clientId = "qQcFD3HqPVrePli7JrUm";// 애플리케이션 클라이언트 아이디값";
+		String clientSecret = "Tv9y2_TXPq";// 애플리케이션 클라이언트 시크릿값";
 		try {
 			String text = URLEncoder.encode(title, "UTF-8");
 			String apiURL = "https://openapi.naver.com/v1/search/movie?query=" + text + "&start=" + start; // json
